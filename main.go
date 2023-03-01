b8c9dbf887555927051f756014c0d27a175dafc6
Bartłomiej Rudecki <ejbtrd@gmail.com>
2023-03-01 22:27:41 +0100

    dtd: Initial telegram message sending

--- main.go
+++ main.go
@@ -1,3 +1,28 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+func main() {
+	time := time.Now().Format("2006-01-02 15:04:05")
+
+	message := fmt.Sprintf("Download stats as of %s in last 24 hours:", time)
+
+	const CHAT_ID int64 = -1001791062372
+
+	bot, err := tgbotapi.NewBotAPI(os.Getenv("TG_BOT_TOKEN"))
+	if err != nil {
+		log.Panic(err)
+	}
+	bot.Debug = true
+	msg := tgbotapi.NewMessage(CHAT_ID, message)
+	if _, err := bot.Send(msg); err != nil {
+		log.Panic(err)
+	}
+}
